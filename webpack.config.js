e3e22196b147e7e4efe61cd89b18d4e3290122c4
NicMilli <nicbass2@gmail.com>
2023-03-14 17:34:52 -0700

    Initial repo setup and dependency installation

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/App.jsx'),
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader',
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
