6ae6efd2e2439fc51357e76e95f0fe1cdafad49c
Brett Tanonaka <tano.brett@gmail.com>
2023-03-16 12:24:10 -0700

    Default reset added in styles.css

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const RelatedItems = () => {
   // linter doesn't like the return for only one element
-  return <div>Hello from RelatedItems</div>;
+  return <div>Hello from RelatedItedddddms</div>;
 };
 
 export default RelatedItems;
