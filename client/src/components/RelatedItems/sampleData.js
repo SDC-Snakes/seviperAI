7335bd3a6d9902900400f0f46e80c236658044ed
Brett Tanonaka <tano.brett@gmail.com>
2023-03-17 15:29:29 -0700

    Created sample data for cards

---
+++ client/src/components/RelatedItems/sampleData.js
@@ -0,0 +1,16 @@
+let sampleData = [
+  {
+    image: '/dist/sampleImages/HCCL25022KLEO01_A.jpg',
+    category: 'Shirts',
+    name: 'Flamboyant cheetah print',
+    price: '$203'
+  },
+  {
+    image: '/dist/sampleImages/T21PANTBK-_2000.jpg',
+    category: 'Pants',
+    name: 'Rugged outdoors',
+    price: '$39'
+  },
+];
+
+exports.sampleData = sampleData;
