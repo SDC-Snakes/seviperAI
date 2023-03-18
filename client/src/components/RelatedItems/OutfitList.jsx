c9dce9b7b108155782fc67f1f775f680bbbe1157
Brett Tanonaka <tano.brett@gmail.com>
2023-03-18 15:44:53 -0700

    Duplicated ItemsList into OutfitList

--- client/src/components/RelatedItems/OutfitList.jsx
+++ client/src/components/RelatedItems/OutfitList.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormatCard from './FormatCard';
+import sampleData from './sampleData';
 
-const OutfitList = () => {
+const OutfitList = ({ itemStyles }) => {
+  const [outfitList, setOutfitList] = useState(sampleData);
 
+  const renderList = (item, index) => {
+    return <FormatCard key={index} name={item.name} image={item.image} image={item.image} price={item.price} itemStyles={itemStyles}/>
+  };
+
+  return(
+    <div>
+      <span className={itemStyles['items-list-title']}>Your outfit</span>
+      <div className={itemStyles['items-list']}>
+        {outfitList.sampleData.map((item, index) => renderList(item, index))}
+      </div>
+    </div>
+  )
 };
 
 export default OutfitList;
--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ItemsList from './ItemsList';
 import itemStyles from './Items.module.css';
+import OutfitList from './OutfitList';
 
 const RelatedItems = () => {
   return (
   <div>
     <ItemsList itemStyles={itemStyles}/>
+    <OutfitList itemStyles={itemStyles}/>
   </div>
   );
 }
