0ee4c97481c58e4803a82f8cdc9985ce2afbe2f7
Brett Tanonaka <tano.brett@gmail.com>
2023-03-18 13:08:45 -0700

    Fixed merge conflicts

--- client/src/components/RelatedItems/ItemsList.jsx
+++ client/src/components/RelatedItems/ItemsList.jsx
@@ -4,7 +4,6 @@ import sampleData from './sampleData';
 
 const ItemsList = (/* id, image, rating, prod_category, prod_name, price */) => {
   const [relatedItemsList, setRelatedItemsList] = useState(sampleData);
-  console.log('relatedItemsList: ', relatedItemsList);
 
   const renderList = (item, index) => {
     return <FormatCard key={index} name={item.name} image={item.image} image={item.image} price={item.price} />
