f319219b5d00a6573247a9f34d2613e7e9cbeadd
Brett Tanonaka <tano.brett@gmail.com>
2023-03-18 15:21:09 -0700

    Added start script to run server/index.js in package.json

--- client/src/components/RelatedItems/FormatCard.jsx
+++ client/src/components/RelatedItems/FormatCard.jsx
@@ -5,9 +5,9 @@ const FormatCard = ({ image, category, name, price }) => (
   <div className={itemStyles['related-items-card']}>
     <img className={itemStyles['card-img']} src={image}/>
     <div>Star rating</div>
-    <h3 className='product-category'>{category}</h3>
-    <h6 className='product-name'>{name}</h6>
-    <p className='card-price'>{price}</p>
+    <h3 className={itemStyles['product-category']}>{category}</h3>
+    <h6 className={itemStyles['product-name']}>{name}</h6>
+    <p className={itemStyles['card-price']}>{price}</p>
   </div>
   )
 
