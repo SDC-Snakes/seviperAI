f587753f3a349ed8fe5c5b3f52d177219d906f43
Majd Saleh <majd.saleh95@gmail.com>
2023-03-20 14:32:04 -0700

    create a new file ThumbnailImageModal.jsx to render review images in a new Modal Window.

--- client/src/components/ReviewsRatings/ReviewTile.jsx
+++ client/src/components/ReviewsRatings/ReviewTile.jsx
@@ -4,24 +4,45 @@ import { useSelector, useDispatch } from 'react-redux';
 import { formatDistanceToNow, parseISO, format } from 'date-fns';
 import QuarterIncStarRating from './QuarterIncStarRating';
 import Report from './Report';
+import ThumbnailImageModal from './ThumbnailImageModal';
+import RNRCSS from './Modal.module.css';
 
 function ReviewTile({ index }) {
   const [helpful, setHelpful] = useState('');
   // send a post request with the helpful state to the API
   const { reviews } = useSelector((state) => state.reviews);
+
+  function renderImageModal() {
+    console.log('renderImage MODAL')
+    //return (<ThumbnailImageModal RNRCSS={RNRCSS} />);
+  }
+  // copied from ThumbnailImageModal
+  const [modalImage, setModalImage] = useState(false); // or false?
+  const toggleModalImage = (inputBool) => (
+    setModalImage(inputBool)
+  );
   return (
     <div>
       {reviews.results[index].rating}
       <QuarterIncStarRating averageRating={reviews.results[index].rating} />
-      <small>{ format(new Date(reviews.results[index].date), 'MMMM dd yyyy') }
+      <small>
+        { format(new Date(reviews.results[index].date), 'MMMM dd yyyy') }
       </small>
       <h5>
-        Review Title Summary: {reviews.results[index].summary}
+        Review Title Summary:
+        {reviews.results[index].summary}
       </h5>
       <p>{reviews.results[index].body}</p>
       {reviews.results[index].photos.map((photo) => (
-        // eslint-disable-next-line jsx-a11y/img-redundant-alt
-        <img key={photo.id} src={photo.url} alt={`Photo ${photo.id}`} />
+        <span key={photo.id}>
+          <img
+            key={photo.id}
+            src={photo.url}
+            alt={`${photo.id}`}
+            onClick={() => { toggleModalImage(true); }}
+          />
+          {modalImage && (<ThumbnailImageModal RNRCSS={RNRCSS} toggleModalImage={toggleModalImage} photo={photo}/>)}
+        </span>
       ))}
       <div>
         Helpful?
