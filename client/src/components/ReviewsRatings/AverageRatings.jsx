0a107300aa2ba85a3dfd4fbb615d3ae06d323f25
Majd Saleh <majd.saleh95@gmail.com>
2023-03-20 11:09:39 -0700

    render percentage of recommendation that are true

--- client/src/components/ReviewsRatings/AverageRatings.jsx
+++ client/src/components/ReviewsRatings/AverageRatings.jsx
@@ -6,17 +6,19 @@ import QuarterStarsAverageRating from './QuarterStarsAverageRating';
 
 function AverageRatings({RNRCSS}) {
   const { meta } = useSelector((state) => state.reviews);
+  console.log('meta in avgRatings', meta)
   const obj = meta.ratings;
   const values = Object.values(obj);
   const totalNumRatings = values.reduce((a, b) => (Number(a) + Number(b)));
   const keys = Object.keys(obj);
   const starRatingPercentages = keys.map((key) => ((obj[key] / totalNumRatings) * 100));
 
+  // recommended percentage calculation
+  let recommendPercent = (Number(meta.recommended.true) / (Number(meta.recommended.true) + Number(meta.recommended.false))) * 100 ;
   return (
     <aside className={RNRCSS['average-ratings-left']}>
       <h3>Product Ratings</h3>
       <QuarterStarsAverageRating />
-      <div>% of reviews recommend this product</div>
       {
         // make sure that the data returned from the API includes
         // all 5 stars always, meaning if no one rated the product
@@ -28,6 +30,7 @@ function AverageRatings({RNRCSS}) {
           </div>
         ))
       }
+      <div>{Math.round(recommendPercent)}% of reviews recommend this product</div>
     </aside>
   );
 }
