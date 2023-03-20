471f0f40ab47bdb06b6c370edc8992c44b78ca8c
Brett Tanonaka <tano.brett@gmail.com>
2023-03-19 18:23:19 -0700

    Testing fetching ratings from API

--- client/src/components/RelatedItems/OutfitList.jsx
+++ client/src/components/RelatedItems/OutfitList.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import FormatCard from './FormatCard';
 import sampleData from './sampleData';
+import { useParams } from "react-router-dom";
+import { useGetRelatedProductInfoQuery } from '../../features/api/apiSlice';
 
 const OutfitList = function ({ itemStyles }) {
   const [outfitList, setOutfitList] = useState(sampleData);
+  const params = useParams();
+  const {
+    data: relatedProducts,
+    isFetching,
+  } = useGetRelatedProductInfoQuery(`${params.productId}`, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  console.log('FormatCard relatedProducts: ', relatedProducts);
 
   const renderList = function (item, index) {
     return (
