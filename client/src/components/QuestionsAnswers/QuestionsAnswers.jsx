46e9cdeb625be6e89d10bd6317b1fcfacf9a7e7b
msong <minseoksong8@gmail.com>
2023-03-21 09:48:54 -0700

    made QnA tiles dynamic fetching data from server

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 // import Search from './subComponents/Search';
 import QuestionsList from './subComponents/QuestionsList';
 // import AnswerModalWindow from './answerModal/AnswerModalWindow';
 import { questionsSample } from './sampleData';
-
-const productId = '40344'; // LATER, SAMPLE NUMBER
 
 function QuestionsAnswers() {
   const params = useParams();
@@ -28,21 +26,20 @@ function QuestionsAnswers() {
         product_id: params.productId,
         page: 1,
         count: 100,
-      }
+      },
     })
       .then((response) => {
-        console.log('QQQQ',response.data);
         setQuestions(response.data.results.sort(sortDescHelpful));
         setLoading(false);
       })
       .catch((err) => {
         setError(err);
         setLoading(false);
-      })
+      });
   }, []);
 
-  if (loading) return <div> Loading...</div>
-  if (error) return <div> Error has occurred while loading</div>
+  if (loading) return <div> Loading...</div>;
+  if (error) return <div> Error has occurred while loading</div>;
   if (!questions) return null;
 
   const loadMoreQs = () => {
