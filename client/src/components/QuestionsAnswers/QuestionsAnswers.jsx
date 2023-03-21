b502b667f829e91e14fdf9b9d5dadae74bb1caec
msong <minseoksong8@gmail.com>
2023-03-21 08:48:37 -0700

    implemented axios get request. Now it fetches data from the server upon initialization

--- client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/src/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
 // import Search from './subComponents/Search';
 import QuestionsList from './subComponents/QuestionsList';
 // import AnswerModalWindow from './answerModal/AnswerModalWindow';
@@ -7,10 +9,41 @@ import { questionsSample } from './sampleData';
 const productId = '40344'; // LATER, SAMPLE NUMBER
 
 function QuestionsAnswers() {
+  const params = useParams();
   const sortDescHelpful = (a, b) => (b.question_helpfulness - a.question_helpfulness);
   // sort according to helpfulness
-  const [questions, setQuestions] = useState(questionsSample.results.sort(sortDescHelpful));
+  // const [questions, setQuestions] = useState(questionsSample.results.sort(sortDescHelpful));
+  const [questions, setQuestions] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // need to declare all the state variables on the top
   const [numberOfQs, setNumberOfQs] = useState(2);
+
+  useEffect(() => {
+    setError(null);
+    setQuestions(null);
+    setLoading(true);
+    axios.get('http://localhost:8080/qa/questions/', {
+      params: {
+        product_id: params.productId,
+        page: 1,
+        count: 100,
+      }
+    })
+      .then((response) => {
+        console.log('QQQQ',response.data);
+        setQuestions(response.data.results.sort(sortDescHelpful));
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) return <div> Loading...</div>
+  if (error) return <div> Error has occurred while loading</div>
+  if (!questions) return null;
 
   const loadMoreQs = () => {
     setNumberOfQs(Math.min(numberOfQs + 2, questions.length));
