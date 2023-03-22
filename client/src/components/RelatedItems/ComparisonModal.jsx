b32e0e7ad05fd52f6dc320710457b00cef5af139
Brett Tanonaka <tano.brett@gmail.com>
2023-03-22 13:17:04 -0700

    Modal lightly styled, closes on button

--- client/src/components/RelatedItems/ComparisonModal.jsx
+++ client/src/components/RelatedItems/ComparisonModal.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import itemStyles from './Items.module.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { newModalState } from '../../features/related/relatedSlice';
 
 function ComparisonModal({ sampleChar }) {
   let { modalOpen, combinedProductFeatures } = useSelector((state) => state.related);
+  const dispatch = useDispatch();
 
   function renderComparison(char, index) {
     return (
@@ -15,24 +17,35 @@ function ComparisonModal({ sampleChar }) {
     );
   }
 
+  function closeModal(e) {
+    e.preventDefault();
+    dispatch(newModalState());
+  }
+
+
+  if (modalOpen) {
   return (
-    { modalOpen } ? (
-      <table>
-        <caption>Comparing</caption>
-        <thead>
-          <tr>
-            <th>Current Product</th>
-            <th>Characteristic</th>
-            <th>Related Product</th>
-          </tr>
-        </thead>
-        <tbody>
-          {combinedProductFeatures.map((char, index) => renderComparison(char, index))}
-        </tbody>
-      </table>
+      <div className={itemStyles.modal}>
+        <div className={itemStyles.overlay}>
+          <table className={itemStyles['modal-content']}>
+            <caption>Comparing</caption>
+            <thead>
+              <tr>
+                <th>Current Product</th>
+                <th>Characteristic</th>
+                <th>Related Product</th>
+              </tr>
+            </thead>
+            <tbody>
+              {combinedProductFeatures.map((char, index) => renderComparison(char, index))}
+            </tbody>
+          </table>
+          <input type='button' className={itemStyles['close-modal']} value="CLOSE MODAL" onClick={closeModal}/>
+        </div>
+      </div>
     )
-      : null
-  );
+  }
+  return null;
 }
 
 export default ComparisonModal;
