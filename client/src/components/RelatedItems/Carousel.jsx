aade4d98f20e90e95124cd1beda3d4987f476749
Brett Tanonaka <tano.brett@gmail.com>
2023-03-23 17:57:53 -0700

    Carousel doesn't have scroll arrows if less than 5 cards, currently hardcoded to display 5

--- client/src/components/RelatedItems/Carousel.jsx
+++ client/src/components/RelatedItems/Carousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import ItemsList from './ItemsList';
@@ -9,6 +9,13 @@ import { newRelatedCarouselIndex } from '../../features/related/relatedSlice';
 function Carousel() {
   let { relatedIndex, related } = useSelector((state) => state.related);
   const dispatch = useDispatch();
+  const [width, setWidth] = useState(window.innerWidth);
+  const [cardsShowing, setCardsShowing] = useState(0);
+
+  // Determine size of screen
+  useEffect(() => {
+    window.addEventListener('resize', () => setWidth(window.innerWidth));
+  }, []);
 
   function nextSlide() {
     dispatch(newRelatedCarouselIndex(relatedIndex === related.length - 1 ? 0 : relatedIndex + 1));
@@ -23,7 +30,10 @@ function Carousel() {
       <div className={itemStyles['items-list-container']}>
         {relatedIndex !== 0 && <FaChevronLeft className={itemStyles['left-arrow']} onClick={prevSlide} />}
         <ItemsList relatedIndex={relatedIndex} />
-        {relatedIndex !== related.length - 5 && <FaChevronRight className={itemStyles['right-arrow']} onClick={nextSlide} />}
+        {(related.length > 5 && relatedIndex !== related.length - 5)
+        && <FaChevronRight className={itemStyles['right-arrow']} onClick={nextSlide} />}
+        {/* {relatedIndex !== related.length
+        && <FaChevronRight className={itemStyles['right-arrow']} onClick={nextSlide} />} */}
       </div>
       <div>
         <FaChevronLeft />
