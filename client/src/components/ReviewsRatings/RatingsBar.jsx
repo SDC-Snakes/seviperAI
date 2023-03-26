d04567fe454ce68f0f19406ff6224515b546f8d4
Majd Saleh <majd.saleh95@gmail.com>
2023-03-26 12:30:07 -0700

    add the hover property to rating bars

--- client/src/components/ReviewsRatings/RatingsBar.jsx
+++ client/src/components/ReviewsRatings/RatingsBar.jsx
@@ -4,10 +4,6 @@ import { newSetRating } from '../../features/reviews/reviewsSlice';
 
 function RatingBar({ index, element, reviewsNum }) {
   const [percentFill, setPercentFill] = useState(element);
-  const transparent = { background: 'transparent' };
-  const grey = { background: 'rgba(4,4,4,.1)' };
-
-  const [color, setcolor] = useState(transparent);
   const dispatch = useDispatch();
   const barRating = useSelector(state => state.reviews.ratingBarSelect);
 
@@ -20,9 +16,6 @@ function RatingBar({ index, element, reviewsNum }) {
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
     <div
       className="rating-bar-container"
-      style={color}
-      onMouseEnter={() => (setcolor(grey))}
-      onMouseLeave={() => (setcolor(transparent))}
       onClick={() => (rateFunc(index + 1))}
     >
       <span style={{ marginRight: '5px' }}>
