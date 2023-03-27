ef1020aad80a2fc79d2bc03143f43065db10d4a1
Brett Tanonaka <tano.brett@gmail.com>
2023-03-27 11:25:17 -0700

    Changed file paths in ItemsList.test imports

--- client/src/__tests__/relatedTests/ItemsList.test.js
+++ client/src/__tests__/relatedTests/ItemsList.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/';
-import FormatCard from '../src/components/RelatedItems/FormatCard';
-import Dropdown from '../src/components/ReviewsRatings/SortReviews';
-import { renderWithProviders } from '../src/components/ProductDetails/__tests__/utils/test-utils';
-import stateStub from '../src/components/ProductDetails/__tests__/proxies/stateProxy';
+import FormatCard from '../../components/RelatedItems/FormatCard';
+import Dropdown from '../../components/ReviewsRatings/SortReviews';
+import { renderWithProviders } from '../utils/test-utils';
+import stateStub from '../proxies/stateProxy';
 
 test('renders Dropdown from Reviews to the page', () => {
   render(<Dropdown />);
