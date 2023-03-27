2baca33a7c6fa5efb610b9b105b938971f515676
Brett Tanonaka <tano.brett@gmail.com>
2023-03-27 15:02:14 -0700

    Synced first test to data stored in Redux state

--- client/src/__tests__/relatedTests/ItemsList.test.js
+++ client/src/__tests__/relatedTests/ItemsList.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import FormatCard from '../../components/RelatedItems/FormatCard';
 import RelatedItems from '../../components/RelatedItems/RelatedItems';
-import { relatedIds, relatedProductPhotos, relatedProductDetails, relatedProductRatings } from '../proxies/itemListProxy';
 import { renderWithProviders } from '../utils/test-utils';
 import stateStub from '../proxies/stateProxy';
+import {
+  relatedIds,
+  relatedProductPhotos,
+  relatedProductDetails,
+  relatedProductRatings,
+} from '../proxies/itemListProxy';
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
@@ -22,11 +29,16 @@ export const handlers = [
     ctx.json(relatedProductPhotos),
     ctx.delay(150),
   )),
-  rest.get('/reviews/meta?product_id=:productId', (req, res, ctx) => res(
-    ctx.json(relatedProductRatings),
-    ctx.delay(150),
-  )),
+  rest.get('/reviews/meta', (req, res, ctx) => {
+    const productRatings = req.url.searchParams.get('product_id');
+    return res(
+      ctx.json(productRatings),
+      ctx.delay(150),
+    );
+  }),
 ];
+
+const server = setupServer(...handlers);
 
 test('renders a product\'s information to its card', () => {
   renderWithProviders(<Router><FormatCard /></Router>, {
