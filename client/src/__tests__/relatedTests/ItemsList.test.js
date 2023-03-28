4fa0bf99f0519d48ba831e8604244a94d295d6ef
Brett Tanonaka <tano.brett@gmail.com>
2023-03-28 11:20:35 -0700

    Reformatted relatedItemsProxy to mimic relatedSlice data

--- client/src/__tests__/relatedTests/ItemsList.test.js
+++ client/src/__tests__/relatedTests/ItemsList.test.js
@@ -6,30 +6,26 @@ import itemStyles from './Items.module.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import Carousel from '../../components/RelatedItems/Carousel';
+import ComparisonModal from '../../components/RelatedItems/ComparisonModal';
 import FormatCard from '../../components/RelatedItems/FormatCard';
-import RelatedItems from '../../components/RelatedItems/RelatedItems';
+import relatedStub from '../../components/RelatedItems/RelatedItems';
 import { renderWithProviders } from '../utils/test-utils';
 import stateStub from '../proxies/stateProxy';
-import {
-  relatedItem,
-  relatedIds,
-  relatedProductPhotos,
-  relatedProductDetails,
-  relatedProductRatings,
-} from '../proxies/itemListProxy';
+import relatedStub from '../proxies/relatedItemsProxy';
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
   rest.get('/products/:productId/related', (req, res, ctx) => res(
-    ctx.json(relatedIds),
+    ctx.json(relatedStub.related.relatedIds),
     ctx.delay(150),
   )),
   rest.get('/products/:productId', (req, res, ctx) => res(
-    ctx.json(relatedProductDetails),
+    ctx.json(relatedStub.relatedItem),
     ctx.delay(150),
   )),
   rest.get('/products/:productId/styles', (req, res, ctx) => res(
-    ctx.json(relatedProductPhotos),
+    ctx.json(relatedStub.relatedItem.photos),
     ctx.delay(150),
   )),
   rest.get('/reviews/meta', (req, res, ctx) => {
@@ -78,18 +74,48 @@ test('renders a product\'s information to its card', async () => {
   expect(await screen.findByText(`$${proxyPrice}`)).toBeInTheDocument();
 });
 
-test('navigates to product\'s page when double clicked', () => {
-  window.location.assign = jest.fn();
+test('comparison modal should be null on load', async () => {
+  renderWithProviders(
+    <Router><ComparisonModal /></Router>,
+    {
+      preloadedState: {
+        combinedProductFeatures: combinedProductFeat,
+      },
+    },
+  );
+  expect(screen.queryByText(combinedProductFeat)).toBeNull();
+});
+
+
+test('comparison modal should appear when icon is clicked', async () => {
   renderWithProviders(
     <Router>
-      <FormatCard className={itemStyles['items-card']} onDoubleClick={window.location.assign} />
+      <ComparisonModal />
+      <FormatCard />
     </Router>,
+    {
+      preloadedState: {
+        combinedProductFeatures: combinedProductFeat,
+      },
+    },
   );
-  const cardComponent = screen.getByTestId("card");
-
-  userEvent.dblClick(cardComponent);
-  expect(window.location.pathname).toContain(`/${relatedItem.product_id}`);
+  screen.logTestingPlaygroundURL();
+  expect(screen.findByText('Fabric: 100% Cotton')).toBeInTheDocument();
+  expect(screen.queryByText(combinedProductFeat)).toBeNull();
 });
+
+// test('navigates to product\'s page when double clicked', () => {
+//   window.location.assign = jest.fn();
+//   renderWithProviders(
+//     <Router>
+//       <FormatCard className={itemStyles['items-card']} onDoubleClick={window.location.assign} />
+//     </Router>,
+//   );
+//   const cardComponent = screen.getByTestId("card");
+
+//   userEvent.dblClick(cardComponent);
+//   expect(window.location.pathname).toContain(`/${relatedItem.product_id}`);
+// });
 
 // test('navigates to product\'s page when double clicked', async () => {
 //   const doubleClick = jest.fn();
