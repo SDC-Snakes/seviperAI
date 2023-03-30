65f89a2f4268e77261a0567916976505561d4a5d
Brett Tanonaka <tano.brett@gmail.com>
2023-03-30 15:37:56 -0700

    Navbar colors and content are created

--- client/src/components/SharedComponents/Navbar.jsx
+++ client/src/components/SharedComponents/Navbar.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 function Navbar({ handleScroll }) {
   return (
-    <div className="flex navbar">
-      <h1>WiredWardrode</h1>
+    <div className="navbar">
+      <h1 className="navbar-title">WiredWardrode</h1>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <h2>Search box</h2>
+      <div className="navbar-headers">
+        <h5 className="navbar-related">Related Items</h5>
+        <h5 className="navbar-qna">Questions & Answers</h5>
+        <h5 className="navbar-reviews">Ratings & Reviews</h5>
+      </div>
+      <input type="search" placeholder="Search" className="navbar-search" />
     </div>
   );
 }
