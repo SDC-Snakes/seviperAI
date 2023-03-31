b8b2978ff3b5c8af0daaf527ed9bca126b1a9a7a
Brett Tanonaka <tano.brett@gmail.com>
2023-03-30 21:20:35 -0700

    Person login icon added

--- client/src/components/SharedComponents/Navbar.jsx
+++ client/src/components/SharedComponents/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './WiredWardrobeLogo.png';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaShoppingCart, FaUser } from 'react-icons/fa';
 
 function Navbar({ handleScroll }) {
   return (
     <div className="navbar">
       <img src={logo} className="navbar-logo" alt="ww-logo" />
-      <h1 className="navbar-title">WiredWardrode</h1>
+      <h1 className="navbar-title">WiredWardrobe</h1>
       <div className="navbar-headers">
         <h5 className="navbar-related navbar-sections">Related Items</h5>
         <h5 className="navbar-qna navbar-sections">Questions & Answers</h5>
@@ -16,6 +16,10 @@ function Navbar({ handleScroll }) {
         <div className="navbar-search-bubble" />
         <FaSearch className="navbar-search-icon" />
         <input type="text" placeholder="Search" className="navbar-search" />
+      </div>
+      <div className="navbar-right-icons-container">
+        <FaShoppingCart className="navbar-shopping-cart navbar-right-icon" />
+        <FaUser className="navbar-person navbar-right-icon" />
       </div>
     </div>
   );
