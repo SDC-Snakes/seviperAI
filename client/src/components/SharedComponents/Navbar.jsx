5bceb8ce4d26c158d92b43c487792c7137febde6
Brett Tanonaka <tano.brett@gmail.com>
2023-03-30 20:48:14 -0700

    Navbar has logo, title, sections  and search bar in place, missing shopping cart

--- client/src/components/SharedComponents/Navbar.jsx
+++ client/src/components/SharedComponents/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import logo from './WiredWardrobeLogo.png';
 import { FaSearch } from 'react-icons/fa';
 
 function Navbar({ handleScroll }) {
   return (
     <div className="navbar">
+      <img src={logo} className="navbar-logo" alt="ww-logo" />
       <h1 className="navbar-title">WiredWardrode</h1>
       <div className="navbar-headers">
-        <h5 className="navbar-related">Related Items</h5>
-        <h5 className="navbar-qna">Questions & Answers</h5>
-        <h5 className="navbar-reviews">Ratings & Reviews</h5>
+        <h5 className="navbar-related navbar-sections">Related Items</h5>
+        <h5 className="navbar-qna navbar-sections">Questions & Answers</h5>
+        <h5 className="navbar-reviews navbar-sections">Ratings & Reviews</h5>
       </div>
       <div className="navbar-search-container">
         <div className="navbar-search-bubble" />
-        <FaSearch />
-        <input type="search" placeholder="Search" className="navbar-search" />
+        <FaSearch className="navbar-search-icon" />
+        <input type="text" placeholder="Search" className="navbar-search" />
       </div>
     </div>
   );
